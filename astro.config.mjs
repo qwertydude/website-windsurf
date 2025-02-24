8fa3a8e9245398d3af4a9b0f3a20110996c86066
Chris Howard <reg@341design.com.au>
2025-02-24 13:58:10 +1100

    chore: switch from Cloudflare to Kinsta configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: cloudflare()
+  output: 'static'
 });
